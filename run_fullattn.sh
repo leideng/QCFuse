b7e5b2d37eeb5c22b3760a3c9a40c789399d49e3
leideng <ldeng.sjtu@gmail.com>
2026-06-16 17:09:53 +0800

    full attn can run with result: Qwen3-8B	hotpotqa	fullcomp	F1=0.6094

---
+++ run_fullattn.sh
@@ -0,0 +1,8 @@
+python blend/sglang_blend_ssd.py \
+  --model Qwen3-8B \
+  --model_dir /public/models \
+  --data_dir data/final_data \
+  --dataset hotpotqa \
+  --baseline fullcomp \
+  --size 200 \
+  --cache_dir cache/qcfuse
