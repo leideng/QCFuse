3daa0c6211f24247370254803872f35b74bb16dd
leideng <ldeng.sjtu@gmail.com>
2026-06-16 17:44:59 +0800

    add 2wikimqa log where QCFuse F1=0.4999 and fullattn F1=0.5379

--- run_fullattn.sh
+++ run_fullattn.sh
@@ -2,7 +2,7 @@ python blend/sglang_blend_ssd.py \
   --model Qwen3-8B \
   --model_dir /public/models \
   --data_dir data/final_data \
-  --dataset hotpotqa \
+  --dataset 2wikimqa \
   --baseline fullcomp \
   --size 200 \
   --cache_dir cache/qcfuse
--- run_qcfuse.sh
+++ run_qcfuse.sh
@@ -2,7 +2,7 @@ python blend/sglang_blend_ssd.py \
   --model Qwen3-8B \
   --model_dir /public/models \
   --data_dir data/final_data \
-  --dataset hotpotqa \
+  --dataset 2wikimqa \
   --baseline ours \
   --size 200 \
   --cache_dir cache/qcfuse
