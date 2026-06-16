3a3c3bb9e35e223486665529d33955c01bfcb1b5
leideng <ldeng.sjtu@gmail.com>
2026-06-16 14:51:44 +0800

    add run qcfuse script

---
+++ run_qcfuse.sh
@@ -0,0 +1,8 @@
+python blend/sglang_blend_ssd.py \
+  --model qwen3-8b \
+  --model_dir models \
+  --data_dir data/final_data \
+  --dataset hotpotqa \
+  --baseline ours \
+  --size 200 \
+  --cache_dir cache/qcfuse
