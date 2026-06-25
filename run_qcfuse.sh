8d373f91e8738d2834a4b6cdf487cc0f73822bac
leideng <ldeng.sjtu@gmail.com>
2026-06-25 12:03:20 +0800

    add new qcfuse running log

--- run_qcfuse.sh
+++ run_qcfuse.sh
@@ -1,7 +1,7 @@
 python blend/sglang_blend_ssd.py \
   --model Qwen3-8B \
   --model_dir /public/models \
-  --data_dir data/final_data \
+  --data_dir data \
   --dataset 2wikimqa \
   --baseline ours \
   --size 200 \
